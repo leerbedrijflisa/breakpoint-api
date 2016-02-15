4979eb5e3174ac61cb1d4d47954284772cbca210
Marv-Meij <meijwaardm@gmail.com>
2016-02-15 09:31:42 +0100

    Empty Project

---
+++ src/Breakpoint-api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace Breakpoint_api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(opts =>
+            {
+                opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseMvc();
+        }
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
