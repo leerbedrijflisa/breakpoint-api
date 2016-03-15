50bb009f98e112b0fb5d5ce821e24fdeb6ac1b1e
Marv-Meij <meijwaardm@gmail.com>
2016-03-15 09:16:30 +0100

    Added the post function and FetchSingleReport, Post still needs adjustment.

--- Lisa.Breakpoint.Api/Database.cs
+++ Lisa.Breakpoint.Api/Database.cs
@@ -3,6 +3,7 @@ using Lisa.Common.WebApi;
 using Microsoft.Extensions.OptionsModel;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +26,41 @@ namespace Lisa.Breakpoint.Api
 
             var results = reports.Select(r => ReportMapper.ToModel(r));
             return results;
+        }
+
+        public async Task<IEnumerable<DynamicModel>> FetchSingleReport(Guid id)
+        {
+            CloudTable table = await Connect();
+
+            var query = new TableQuery<DynamicEntity>().Where(TableQuery.GenerateFilterConditionForGuid("Id", QueryComparisons.Equal, id));
+            var report = await table.ExecuteQuerySegmentedAsync(query, null);
+
+            if (report.Count() == 0)
+            {
+                return null;
+            }
+
+            var result = report.Select(r => ReportMapper.ToModel(r));
+            var result2 = result.FirstOrDefault();
+            return result;
+        }
+
+        public async Task<DynamicModel> SaveReport(dynamic report)
+        {
+            CloudTable table = await Connect();
+
+            dynamic reportEntity = ReportMapper.ToEntity(report);
+
+            reportEntity.PartitionKey = reportEntity.Project;
+            reportEntity.RowKey = reportEntity.id;
+
+            var action = TableOperation.Insert(reportEntity);
+
+            await table.ExecuteAsync(action);
+
+            var result = ReportMapper.ToModel(reportEntity);
+
+            return result;
         }
 
         private async Task<CloudTable> Connect()
--- Lisa.Breakpoint.Api/ReportController.cs
+++ Lisa.Breakpoint.Api/ReportController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Lisa.Common.WebApi;
+using Microsoft.AspNet.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Lisa.Breakpoint.Api
 {
     [Route("/reports/")]
-    public class ReportController
+    public class ReportController : Controller
     {
         public ReportController(Database database)
         {
@@ -16,6 +18,29 @@ namespace Lisa.Breakpoint.Api
         {
             var reports = await _db.FetchReports();
             return new HttpOkObjectResult(reports);
+        }
+
+        [HttpGet("{id}", Name = "SingleReport")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            object report = await _db.FetchSingleReport(id);
+
+            if (report == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            return new HttpOkObjectResult(report);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] DynamicModel report)
+        {
+            dynamic result = await _db.SaveReport(report);
+
+            string location = Url.RouteUrl("SingleReport", new { id = result.id }, Request.Scheme);
+
+            return new CreatedResult(location, result);
         }
 
         private Database _db;
