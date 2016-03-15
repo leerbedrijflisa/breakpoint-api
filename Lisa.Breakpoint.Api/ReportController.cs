a89e36c6c201f7a42de9f31f3f9ede6b6e3d1987
Simon Besseling <99029652@mydavinci.nl>
2016-03-15 14:47:24 +0100

    Start Sort (To be finished & tested tomorrow... Or sumeting)

--- Lisa.Breakpoint.Api/ReportController.cs
+++ Lisa.Breakpoint.Api/ReportController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lisa.Breakpoint.Api
@@ -12,11 +14,24 @@ namespace Lisa.Breakpoint.Api
         }
 
         [HttpGet]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> Get([FromQuery] string sort, [FromQuery] string order)
         {
             var reports = await _db.FetchReports();
-            return new HttpOkObjectResult(reports);
+            if (sort == null || order == null)
+            {
+                return new HttpOkObjectResult(reports);
+            }
+
+            var result = ReportSorter.Sort(reports, sort, order);
+            return new HttpOkObjectResult(result);
         }
+
+        //[HttpGet]
+        //public async Task<ActionResult> Get([FromQuery]string sort, [FromQuery]string order)
+        //{
+        //    var reports = await _db.FetchReports();
+        //    return new HttpOkObjectResult(sort);
+        //}
 
         private Database _db;
     }
---
+++ Lisa.Breakpoint.Api/ReportSorter.cs
@@ -0,0 +1,21 @@
+﻿using Lisa.Common.WebApi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lisa.Breakpoint.Api
+{
+    public static class ReportSorter
+    {
+        public static List<DynamicModel> Sort(IEnumerable<DynamicModel> reports, string sort, string order)
+        {
+            if (sort.ToLower() == "project")
+            {
+                if (order.ToLower() == "asc")
+                {
+                    return reports.AsQueryable().OrderBy(r => r[sort]).ToList();
+                }
+            }
+            return null;
+        }
+    }
+}
