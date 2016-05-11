f128d5dda0339571b9bc9734fc710ddac9f1f91d
Marv-Meij <meijwaardm@gmail.com>
2016-05-11 13:40:05 +0200

    Added Role validate

--- Lisa.Breakpoint.Api/MemberShipsValidator.cs
+++ Lisa.Breakpoint.Api/MemberShipsValidator.cs
@@ -1,19 +1,46 @@
 ﻿using Lisa.Common.WebApi;
+using System.Linq;
 
 namespace Lisa.Breakpoint.Api
 {
     public class MemberShipsValidator : Validator
     {
+        private void ValidateRole(string fieldName, object value)
+        {
+            string[] status = new string[] { "developer", "tester", "manager"};
+
+            if (value != null)
+            {
+                if (!status.Contains(value))
+                {
+                    var error = new Error
+                    {
+                        Code = ErrorCode.EmptyValue,
+                        Message = $"In field '{fieldName}' only the values: developer, tester and manager are allowed.",
+
+                        Values = new
+                        {
+                            Field = fieldName,
+                            Value = value,
+                            ExpectedValues = status
+                        }
+                    };
+                    Result.Errors.Add(error);
+                }
+            }
+        }
+
         protected override void ValidateModel()
         {
+            Ignore("id");
             Required("userName", NotEmpty, TypeOf(DataTypes.String));
             Required("project", NotEmpty, TypeOf(DataTypes.String));
-            Required("role", NotEmpty, TypeOf(DataTypes.String));
+            Required("role", NotEmpty, ValidateRole, TypeOf(DataTypes.String));
         }
 
         protected override void ValidatePatch()
         {
-            
+
         }
     }
 }
