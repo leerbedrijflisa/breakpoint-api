f824a7551d23e051cc7e16b7babf46414f3b1d7e
Marv-Meij <meijwaardm@gmail.com>
2016-05-20 12:19:24 +0200

    Refactor  role validation.

--- Lisa.Breakpoint.Api/MemberShipValidator.cs
+++ Lisa.Breakpoint.Api/MemberShipValidator.cs
@@ -1,35 +1,9 @@
 ﻿using Lisa.Common.WebApi;
-using System.Linq;
 
 namespace Lisa.Breakpoint.Api
 {
     public class MemberShipValidator : Validator
     {
-        private void ValidateRole(string fieldName, object value)
-        {
-            string[] status = new string[] { "developer", "tester", "manager"};
-
-            if (value != null)
-            {
-                if (!status.Contains(value))
-                {
-                    var error = new Error
-                    {
-                        Code = ErrorCode.EmptyValue,
-                        Message = $"In field '{fieldName}' only the values: developer, tester and manager are allowed.",
-
-                        Values = new
-                        {
-                            Field = fieldName,
-                            Value = value,
-                            ExpectedValues = status
-                        }
-                    };
-                    Result.Errors.Add(error);
-                }
-            }
-        }
-
         public static object MembershipError(DynamicModel membership)
         {
             var error = new Error
@@ -47,7 +21,7 @@ namespace Lisa.Breakpoint.Api
             Ignore("id");
             Required("userName", NotEmpty, TypeOf(DataTypes.String));
             Required("project", NotEmpty, TypeOf(DataTypes.String));
-            Required("role", NotEmpty, ValidateRole, TypeOf(DataTypes.String));
+            Required("role", NotEmpty, OneOf(ValidationOptions.CaseSensitive, "developer", "tester", "manager"), TypeOf(DataTypes.String));
         }
 
         protected override void ValidatePatch()
