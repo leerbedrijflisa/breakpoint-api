6f2f4726946a88be25496b98c8c630272e42ea14
Marv-Meij <meijwaardm@gmail.com>
2016-05-20 10:31:11 +0200

    Hotfix from develop also in alpha 4

--- Lisa.Breakpoint.Api/ReportValidator.cs
+++ Lisa.Breakpoint.Api/ReportValidator.cs
@@ -5,38 +5,13 @@ namespace Lisa.Breakpoint.Api
 {
     public class ReportValidator : Validator
     {
-        private void ValidateArray(string fieldName, object value)
-        {
-            string[] status = new string[] { "open", "fixed", "closed", "won't fix", "won't fix (approved)" };
-
-            if (value != null)
-            {
-                if (!status.Contains(value))
-                {
-                    var error = new Error
-                    {
-                        Code = ErrorCode.EmptyValue,
-                        Message = $"In field '{fieldName}' only the values: open, fixed, closed, won't fix and won't fix (approved) are allowed.",
-                         
-                        Values = new
-                        {
-                            Field = fieldName,
-                            Value = value,
-                            ExpectedValues = status
-                        }
-                    };
-                    Result.Errors.Add(error);
-                }
-            }
-        }
-
         protected override void ValidateModel()
         {
             Ignore("id");
             Required("title", NotEmpty, TypeOf(DataTypes.String));
             Required("project", NotEmpty, TypeOf(DataTypes.String));
             Optional("assignee", TypeOf(DataTypes.String));
-            Optional("status", NotEmpty, ValidateArray);
+            Optional("status", NotEmpty, OneOf("open", "fixed", "closed", "wontFix", "wontFixApproved"));
             Ignore("reported");
         }
 
