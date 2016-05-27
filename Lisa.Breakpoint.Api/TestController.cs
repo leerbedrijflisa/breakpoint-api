cb2f7024747cec529cbc276970ae065de746ef56
Jason Roffel <jasonroffel@hotmail.nl>
2016-05-27 10:15:06 +0200

    Pong is needed

---
+++ Lisa.Breakpoint.Api/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Keep this in, it is for testing
+namespace Lisa.Breakpoint.Api
+{
+    [Route("/test/")]
+    public class TestController
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkObjectResult("Pong");
+        }
+    }
+}
